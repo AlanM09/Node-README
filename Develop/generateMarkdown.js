e6b72e2a76f701faeb598b38c72597e4ac14a533
Your Name <alanmayer@email.arizona.edu>
2021-03-24 12:52:03 -0700

    add

---
+++ Develop/generateMarkdown.js
@@ -0,0 +1,36 @@
+// function to generate markdown for README
+function generateMarkdown(data) {
+  return `# ${data.titleOfProject}
+![License badge](https://img.shields.io/badge/license-${data.licenses}-green)
+## Description 
+${data.projectDescription}
+Make sure to include a picture of your project. 
+## License
+The license/licenses used with this project are ${data.licenses}.
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+The command that should be run to install dependencies is ${data.commandDependency}.
+## Usage 
+${data.repoInfo}
+## Credits
+The creator of this project's email and GitHub is below. 
+- ${data.emailAddress}
+- ${data.gitHubUsername}
+## Contributing
+${data.contributingInfo}
+## Tests
+Use the ${data.commandTests} command to run a test.
+## Questions
+If you have any questions about this project, please contact the creator of the project. 
+- ${data.emailAddress}
+- ${data.gitHubUsername}
+`;
+}
+
+module.exports = generateMarkdown;
---
+++ Develop/index.js
@@ -0,0 +1,150 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./generateMarkdown')
+const path = require('path');
+
+console.log('Hello! Create a README for your project by answering the following questions');
+
+// array of questions for user
+const questions = () => {
+    return inquirer.prompt([{
+            type: 'input',
+            name: 'gitHubUsername',
+            message: 'What is your GitHub username?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your GitHub username.');
+                }
+                return false;
+            }
+        },
+        {
+            type: 'input',
+            name: 'emailAddress',
+            message: 'What is your email address?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your email address.');
+                }
+                return false;
+            }
+        },
+
+        {
+            type: 'input',
+            name: 'titleOfProject',
+            message: 'What is the title of your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter the title of your project.');
+                }
+                return false;
+            }
+        }, {
+            type: 'input',
+            name: 'projectDescription',
+            message: 'Please write a short description of your project',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a description of your project.');
+                }
+                return false;
+            }
+        }, {
+            type: 'checkbox',
+            name: 'licenses',
+            message: 'What kind of license should your project have?',
+            choices: [{
+                    name: 'MIT',
+                },
+                {
+                    name: 'APACHE2.0',
+                },
+                {
+                    name: 'GPL3.0',
+                },
+                {
+                    name: 'BSD3',
+                },
+                {
+                    name: 'None',
+                },
+            ]
+        }, {
+            type: 'input',
+            name: 'commandDependency',
+            message: 'What command should be run to install dependencies?',
+            default: 'npm i',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a command.');
+                }
+                return false;
+            }
+        }, {
+            type: 'input',
+            name: 'commandTests',
+            message: 'What command should be run to run tests?',
+            default: 'npm test',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a command.');
+                }
+                return false;
+            }
+        }, {
+            type: 'input',
+            name: 'repoInfo',
+            message: 'What does the user need to know about using the repo?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter a message to the user.');
+                }
+                return false;
+            }
+        }, {
+            type: 'input',
+            name: 'contributingInfo',
+            message: 'What does the user need to know about contributing to the repo?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('Please enter instructions for the user.');
+                }
+                return false;
+            }
+        },
+
+    ])
+};
+
+// function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
+}
+// function to initialize program
+function init() {
+    questions()
+        .then(answers => {
+            writeToFile('Develop/README.md', generateMarkdown({
+                ...answers
+            }));
+        })
+}
+
+init();
